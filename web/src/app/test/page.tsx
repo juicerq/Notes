82295c5eaa92e7ac2c85c1a736bbb99774f74fb1
pedrinhopagan <117399038+pedrinhopagan@users.noreply.github.com>
2023-05-28 21:13:28 -0300

    added transition to note

--- web/src/app/test/page.tsx
+++ web/src/app/test/page.tsx
@@ -1,9 +1,12 @@
+import { NewNoteForm } from '@/components/NewNoteForm'
+
 export default function test() {
   const lorem =
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rationeillum delectus eius sequi dolores vitae provident ut facere aut,pariatur labore, porro quia ab dolor, repudiandae doloribus. Neque,cumque expedita! Earum eius, saepe cum sint non delectus, at,repudiandae reiciendis eveniet ea blanditiis officia. Recusandaealiquid aperiam blanditiis ex impedit molestiae, eaque doloribusalias, voluptatibus, nihil nobis minima ea a.'
 
   return (
     <div className="p-12 flex justify-center items-center bg-zinc-900">
+      <NewNoteForm />
       <div className="text-zinc-300 flex flex-col items-center gap-8">
         <h2 className="text-2xl font-bold">Título da Nota</h2>
         <p className="text-xl indent-4 leading-relaxed">{lorem}</p>
