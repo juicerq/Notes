5b00c9869af5f29fb6ffaa44a27b002c167c8634
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-06-02 12:53:45 -0300

    contact layout

--- web/src/app/contact/page.tsx
+++ web/src/app/contact/page.tsx
@@ -12,11 +12,11 @@ import { Textarea } from '@/components/ui/textarea'
 
 export default function Contact() {
   return (
-    <div className="w-2/4 h-full mt-48 flex justify-items-center gap-3 flex-col">
-      <h1 className="text-4xl tracking-wider py-2 text-center uppercase font-bold text-mainPalette-text">
-        Envie um <span className="highlight">email</span> para nós
+    <div className="w-full h-screen flex justify-center items-center flex-col">
+      <h1 className="text-4xl tracking-wider text-center uppercase font-bold text-mainPalette-text">
+        Entre em <span className="highlight">contato</span> conosco
       </h1>
-      <form className=" flex flex-col gap-5 flex-1 p-10">
+      <form className="w-1/2 flex flex-col gap-5 pt-5">
         <div className="flex w-full gap-10 flex-wrap py-4 tracking-wide">
           <div className="contact-input-container">
             <label
@@ -28,8 +28,11 @@ export default function Contact() {
             <Input placeholder="Insira o seu nome" id="contact-name" />
           </div>
 
-          <div className="flex w-56  flex-col gap-1">
-            <label htmlFor="input02" className="text-lg text-mainPalette-text">
+          <div className="flex w-60 flex-col gap-1">
+            <label
+              htmlFor="contact-style"
+              className="text-lg text-mainPalette-text"
+            >
               Tipo de site
             </label>
             <Select>
@@ -37,25 +40,16 @@ export default function Contact() {
                 <SelectValue placeholder="Modelos" />
               </SelectTrigger>
               <SelectContent className="bg-mainPalette-bg text-mainPalette-primaryButton">
-                <SelectItem value="dark">Blog</SelectItem>
-                <SelectItem value="dark">E-commerce</SelectItem>
-                <SelectItem value="dark">Informativo</SelectItem>
-                <SelectItem value="dark">Landing Page</SelectItem>
-                <SelectItem value="light">Portfolio</SelectItem>
+                <SelectItem value="blog">Blog</SelectItem>
+                <SelectItem value="e-commerce">E-commerce</SelectItem>
+                <SelectItem value="informativo">Informativo</SelectItem>
+                <SelectItem value="landingpage">Landing Page</SelectItem>
+                <SelectItem value="portfolio">Portfolio</SelectItem>
               </SelectContent>
             </Select>
           </div>
 
-          <div className="contact-input-container">
-            <label
-              htmlFor="contact-textarea"
-              className="text-lg text-mainPalette-text"
-            >
-              Digite sua mensagem
-            </label>
-            <Input id="input02" className="contact-input" />
-          </div>
-          <div className="flex w-56 flex-col gap-1">
+          <div className="flex w-60 flex-col gap-1">
             <label htmlFor="input02" className="text-lg text-mainPalette-text">
               Email para contato
             </label>
@@ -65,7 +59,11 @@ export default function Contact() {
             <label htmlFor="input02" className="text-lg text-mainPalette-text">
               Se tiver mais informações ou se tem alguma dúvida insira aqui ↓
             </label>
-            <Textarea id="textarea" className="contact-input" />
+            <Textarea
+              id="textarea"
+              placeholder="Insira sua mensagem"
+              className="contact-input resize-none"
+            />
           </div>
         </div>
       </form>
