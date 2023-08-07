64241ff01b8c8fa9d89e6052d117811ff59c7153
pedrinhopagan <117399038+pedrinhopagan@users.noreply.github.com>
2023-08-07 17:15:30 -0300

    created about section

--- web/src/components/Main/MainComponents/Feature.tsx
+++ web/src/components/Main/MainComponents/Feature.tsx
@@ -8,14 +8,12 @@ interface FeatureProps {
 
 export default function Feature(props: FeatureProps) {
   return (
-    <div className="item flex flex-col justify-start gap-4 rounded bg-mainPalette-bgAlt p-6 py-8 text-white lg:w-[400px] lg:flex-none lg:bg-transparent xl:w-[300px] 2xl:w-[400px]">
+    <div className="item flex min-h-[340px] flex-col justify-start gap-4 rounded bg-mainPalette-bgAlt px-6 py-8 text-white md:min-h-[260px] lg:w-[250px] lg:flex-none lg:bg-transparent xl:w-[320px] 2xl:w-[400px]">
       <div className="flex-center h-12 w-12 justify-center rounded-full bg-mainPalette-primaryButton p-1 text-mainPalette-bgAlt">
         {props.icon}
       </div>
-      <h1 className="font-main text-xl lg:text-2xl xl:text-xl">
-        {props.title}
-      </h1>
-      <p className="text-lg text-zinc-300 md:text-justify xl:text-base 2xl:text-lg">
+      <h2 className="font-main text-lg xl:text-xl">{props.title}</h2>
+      <p className="text-lg text-zinc-500 xl:text-justify">
         {props.description}
       </p>
     </div>
