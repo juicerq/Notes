f622e98eeba7b2fadc847d5100542099c74e0e6e
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-08-07 17:38:57 -0300

    Update FooterInfo.tsx

--- web/src/components/Main/MainComponents/FooterInfo.tsx
+++ web/src/components/Main/MainComponents/FooterInfo.tsx
@@ -11,11 +11,11 @@ export default function FooterInfo(props: FooterInfoProps) {
       <div className="flex items-center gap-3">
         <h1 className="relative text-xl tracking-wider xl:text-xl">
           {props.title}
-          <div className="duration-400 absolute -bottom-1 h-0.5 w-0 bg-mainPalette-bg transition-all group-hover:w-full" />
+          <div className="duration-400 absolute -bottom-1 h-0.5 w-0 bg-zinc-400 transition-all group-hover:w-full" />
         </h1>
       </div>
       <div className="">
-        <p className="duration-400 text-justify text-sm text-gray-300 transition-all group-hover:text-white">
+        <p className="duration-400 text-justify text-sm text-gray-300 transition-all group-hover:text-zinc-400">
           {props.children}
         </p>
       </div>
