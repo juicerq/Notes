5ae205cfc57f1f1129210970e0eccd18f428fd26
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-08-07 13:35:57 -0300

    features css

--- web/src/components/Main/Features.tsx
+++ web/src/components/Main/Features.tsx
@@ -3,43 +3,41 @@ import Feature from './MainComponents/Feature'
 
 export default function Features() {
   return (
-    <div className="section-spacing flex flex-col items-center justify-center gap-20 bg-mainPalette-bgAlt">
+    <div className="section-spacing flex flex-col items-center justify-center gap-20">
       <div
         id="features"
-        className="flex-center bg-palette-outerSpace flex-col xl:gap-10"
+        className="flex w-screen snap-x snap-mandatory scroll-p-3 gap-3 overflow-auto p-3 md:w-full lg:snap-none lg:scroll-p-0 lg:flex-wrap lg:justify-around lg:gap-6 lg:overflow-hidden lg:rounded-lg lg:bg-mainPalette-bgAlt lg:py-12 xl:justify-around"
       >
-        <div className="flex w-screen snap-x snap-mandatory scroll-p-3 gap-3 overflow-auto p-3 md:w-full lg:snap-none lg:scroll-p-0 lg:flex-wrap lg:justify-around lg:gap-6 lg:overflow-hidden lg:rounded-lg lg:bg-mainPalette-bgAlt lg:py-12 xl:justify-around">
-          <Feature
-            icon={<Atom size={35} />}
-            title="Titulo"
-            description="description"
-          />
-          <Feature
-            icon={<Atom size={35} />}
-            title="Titulo"
-            description="description"
-          />
-          <Feature
-            icon={<Atom size={35} />}
-            title="Titulo"
-            description="description"
-          />
-          <Feature
-            icon={<Atom size={35} />}
-            title="Titulo"
-            description="description"
-          />
-          <Feature
-            icon={<Atom size={35} />}
-            title="Titulo"
-            description="description"
-          />
-          <Feature
-            icon={<Atom size={35} />}
-            title="Titulo"
-            description="description"
-          />
-        </div>
+        <Feature
+          icon={<Atom size={35} />}
+          title="Animações Cativantes"
+          description="Integre animações sutis e envolventes ao design do seu site para criar uma experiência visualmente atraente e dinâmica."
+        />
+        <Feature
+          icon={<Atom size={35} />}
+          title="Design Atrativo & Intuitivo"
+          description="Um design visualmente atraente e uma navegação intuitiva facilitam a interação do usuário com o site e tornam a experiência mais agradável."
+        />
+        <Feature
+          icon={<Atom size={35} />}
+          title="Navegação Clara"
+          description="Uma estrutura de menu clara e uma hierarquia de páginas bem definida ajudam os usuários a encontrar facilmente o que estão procurando."
+        />
+        <Feature
+          icon={<Atom size={35} />}
+          title="Integração com Redes Sociais"
+          description=" Facilite o compartilhamento de conteúdo por meio de botões de redes sociais e exiba feeds ou links para suas páginas de redes sociais."
+        />
+        <Feature
+          icon={<Atom size={35} />}
+          title="Formulário de Contato"
+          description="Forneça formulários de contato, chat ao vivo ou outras opções de interação para que os usuários possam se comunicar diretamente com você."
+        />
+        <Feature
+          icon={<Atom size={35} />}
+          title="Responsividade"
+          description="O site deve se adaptar automaticamente a diferentes dispositivos e tamanhos de tela, garantindo uma experiência consistente em smartphones, tablets e desktops."
+        />
       </div>
     </div>
   )
--- web/src/components/Main/MainComponents/Feature.tsx
+++ web/src/components/Main/MainComponents/Feature.tsx
@@ -9,16 +9,14 @@ interface FeatureProps {
 export default function Feature(props: FeatureProps) {
   return (
     <div className="item flex flex-col justify-start gap-4 rounded bg-mainPalette-bgAlt p-6 py-8 text-white lg:w-[400px] lg:flex-none lg:bg-transparent xl:w-[300px] 2xl:w-[400px]">
-      <div className="flex-center h-12 w-12 justify-center rounded-full bg-mainPalette-accent p-1 text-mainPalette-bgAlt">
+      <div className="flex-center h-12 w-12 justify-center rounded-full bg-mainPalette-primaryButton p-1 text-mainPalette-bgAlt">
         {props.icon}
       </div>
-      {/* <Atom className="h-12 w-12 rounded-full bg-mainPalette-accent p-1 text-mainPalette-bgAlt" /> */}
       <h1 className="font-main text-xl lg:text-2xl xl:text-xl">
-        Animações Cativantes & Responsivas
+        {props.title}
       </h1>
-      <p className="text-lg text-zinc-300 xl:text-base 2xl:text-lg">
-        Lorem ipsum texto oi bão bora que dia é esse mais texto para deixar
-        lindo
+      <p className="text-lg text-zinc-300 md:text-justify xl:text-base 2xl:text-lg">
+        {props.description}
       </p>
     </div>
   )
