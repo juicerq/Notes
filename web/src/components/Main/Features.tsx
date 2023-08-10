25154425c1724d6733d2f0d6ef5d213a727f0a93
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-08-10 12:46:30 -0300

    feature animation

--- web/src/components/Main/Features.tsx
+++ web/src/components/Main/Features.tsx
@@ -27,15 +27,18 @@ export default function Features() {
         <div className="relative flex w-screen snap-x snap-mandatory scroll-p-3 gap-3 overflow-auto p-3 lg:w-full lg:snap-none lg:scroll-p-0 lg:flex-wrap lg:justify-between lg:gap-0 lg:overflow-hidden lg:p-0  xl:justify-around">
           <Feature
             icon={<Component size={35} />}
+            delay={0}
             title="Design Atrativo & Intuitivo"
             description="Um design visualmente atraente e uma navegação intuitiva facilitam a interação do usuário com o site e tornam a experiência mais agradável."
           />
           <Feature
+            delay={1}
             icon={<Milestone size={35} />}
             title="Navegação Clara"
             description="Uma estrutura de menu clara e uma hierarquia de páginas bem definida ajudam os usuários a encontrar facilmente o que estão procurando."
           />
           <Feature
+            delay={2}
             icon={<MdOutlineAnimation size={35} />}
             title="Animações Cativantes"
             description="Integre animações sutis e envolventes ao design do seu site para criar uma experiência visualmente atraente e dinâmica."
@@ -47,16 +50,19 @@ export default function Features() {
         </div>
         <div className="flex w-screen snap-x snap-mandatory scroll-p-3 gap-3 overflow-auto p-3 lg:w-full lg:snap-none lg:scroll-p-0 lg:flex-wrap lg:justify-between lg:gap-0 lg:overflow-hidden lg:p-0  lg:pb-12 xl:justify-around">
           <Feature
+            delay={3}
             icon={<Workflow size={35} />}
             title="Integração com Redes Sociais"
             description=" Facilite o compartilhamento de conteúdo por meio de botões de redes sociais e exiba feeds ou links para suas páginas de redes sociais."
           />
           <Feature
+            delay={4}
             icon={<FileText size={35} />}
             title="Formulário de Contato"
             description="Forneçeremos formulários de contato personalizados para garantir a geração de leads de acordo com suas necessidades."
           />
           <Feature
+            delay={5}
             icon={<MonitorSmartphone size={35} />}
             title="Responsividade"
             description="O seu site irá se adaptar automaticamente a diferentes dispositivos e tamanhos de tela, garantindo uma experiência consistente em smartphones, tablets e desktops."
--- web/src/components/Main/MainComponents/Feature.tsx
+++ web/src/components/Main/MainComponents/Feature.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
 
 interface FeatureProps {
   icon: ReactNode
   title: string
   description: string
+  delay: number
 }
 
 export default function Feature(props: FeatureProps) {
+  const animationDelay = props.delay * 0.225
   return (
-    <div className="item flex min-h-[340px] flex-col justify-start gap-4 rounded bg-mainPalette-bgAlt px-6 py-8 text-white md:min-h-[260px] lg:w-[250px] lg:flex-none lg:bg-transparent xl:w-[320px] 2xl:w-[400px]">
+    <motion.div
+      whileInView={{ opacity: [0, 1], y: [50, 0] }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.4, ease: 'easeInOut', delay: animationDelay }}
+      className="item flex min-h-[340px] flex-col justify-start gap-4 rounded bg-mainPalette-bgAlt px-6 py-8 text-white md:min-h-[260px] lg:w-[250px] lg:flex-none lg:bg-transparent xl:w-[320px] 2xl:w-[400px]"
+    >
       <div className="flex-center h-12 w-12 justify-center rounded-full bg-mainPalette-primaryButton p-1 text-mainPalette-text">
         {props.icon}
       </div>
@@ -16,6 +26,6 @@ export default function Feature(props: FeatureProps) {
       <p className="text-lg text-zinc-400 xl:text-justify">
         {props.description}
       </p>
-    </div>
+    </motion.div>
   )
 }
