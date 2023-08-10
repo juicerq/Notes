48fa35a7d3a4c88ebb1bd2df09fafb883625bd25
Pedro Pagan <pedroantoniobonaldo@gmail.com>
2023-08-10 16:45:47 -0300

    about options

--- web/src/components/Main/MainComponents/AboutOptions.tsx
+++ web/src/components/Main/MainComponents/AboutOptions.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import Option from './Option'
+import { motion } from 'framer-motion'
 
 export default function AboutOptions() {
   const [optionIndex, setOptionIndex] = useState(1)
@@ -78,20 +79,73 @@ export default function AboutOptions() {
         </>
       ) : (
         // Opção 2
-        <>
+        <div className="flex flex-col gap-12">
           <Option
             title="Contato Direto"
             buttonName="Contato"
             buttonLink="contact"
           >
             <p className="text-lg leading-relaxed text-zinc-400">
-              {' '}
-              A segunda opção é entrar em contato diretamente conosco via
-              WhatsApp ou e-mail. Retornaremos o contato com algumas opções de
-              serviço a partir do que você tiver interesse.
+              A segunda é nos enviar uma mensagem pelo nosso formulário,
+              acessível pelo botão abaixo. Lá, você pode nos contar mais sobre o
+              que você está buscando e até mesmo pedir um{' '}
+              <span className="text-mainPalette-primaryButton">
+                serviço personalizado
+              </span>
+              , se quiser. Estamos aqui para te ajudar!
             </p>
           </Option>
-        </>
+          <motion.div
+            viewport={{ once: true }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
+            className="flex flex-col gap-6"
+            key={'3'}
+          >
+            <p className="text-lg leading-relaxed text-zinc-400">
+              Outra alternativa é entrar em contato diretamente pelo WhatsApp ou
+              enviar um email para compartilhar{' '}
+              <span className="text-mainPalette-primaryButton">
+                seus planos e ideias
+              </span>
+              . Assim que recebermos sua mensagem, responderemos o mais rápido
+              possível, esclarecendo suas dúvidas e atendendo às suas
+              necessidades.
+            </p>
+
+            <motion.div
+              viewport={{ once: true }}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: [0, 1], y: [50, 0] }}
+              transition={{
+                duration: 0.4,
+                ease: 'easeInOut',
+                delay: 0.6,
+              }}
+              className="flex items-center gap-6 text-mainPalette-bg"
+            >
+              <a
+                href={
+                  'https://api.whatsapp.com/send?phone=5519997855562&text=Ol%C3%A1%21%20Estou%20interessado%28a%29%20em%20um%20site%21%20Gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os.%20Aguardo%20sua%20resposta.%20Obrigado%21'
+                }
+                target="_blank"
+                className=" flex items-center justify-center gap-2 rounded-md underline underline-offset-4 transition-all duration-150 lg:hover:text-zinc-400"
+              >
+                .WhatsApp
+              </a>
+              <a
+                href={
+                  'https://api.whatsapp.com/send?phone=5519997855562&text=Ol%C3%A1%21%20Estou%20interessado%28a%29%20em%20um%20site%21%20Gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os.%20Aguardo%20sua%20resposta.%20Obrigado%21'
+                }
+                target="_blank"
+                className=" flex items-center justify-center gap-2 rounded-md underline underline-offset-4 transition-all duration-150 lg:hover:text-zinc-400"
+              >
+                .Gmail
+              </a>
+            </motion.div>
+          </motion.div>
+        </div>
       )}
     </div>
   )
