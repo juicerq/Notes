4930df30c3f309f56a40e8f075355634fd3f3be8
Júlio Cerqueira <julio.cerqueiira@gmail.com>
2023-08-24 21:04:51 -0300

    Update page.tsx

--- web/src/app/contact/page.tsx
+++ web/src/app/contact/page.tsx
@@ -2,15 +2,9 @@
 
 import PageTransition from '@/components/general/PageTransition'
 import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+
 import { Textarea } from '@/components/ui/textarea'
-import { Mail, Phone, User, Pen, LayoutTemplate } from 'lucide-react'
+import { Mail, Phone, User, Pen } from 'lucide-react'
 
 export default function Contact() {
   return (
@@ -21,6 +15,7 @@ export default function Contact() {
           <span className="text-mainPalette-primaryButton">orçamento</span>{' '}
           agora mesmo
         </h1>
+
         <form className="mt-5 flex w-3/4 flex-col gap-7 indent-1 text-lg text-mainPalette-text lg:w-2/3 2xl:w-1/2">
           <div className="flex w-full flex-wrap gap-10 py-4 tracking-wide">
             <div className="contact-input-container">
@@ -28,7 +23,11 @@ export default function Contact() {
                 <User size={18} className="opacity-80" />
                 <label htmlFor="contact-name">Nome</label>
               </div>
-              <Input placeholder="Insira o seu nome" id="contact-name" />
+              <Input
+                placeholder="Insira o seu nome"
+                id="contact-name"
+                className="bg-zinc-300"
+              />
             </div>
 
             <div className="contact-input-container">
@@ -39,7 +38,7 @@ export default function Contact() {
               <Input
                 id="contact-email"
                 placeholder="Insira seu email"
-                className="contact-input"
+                className="bg-zinc-300"
               />
             </div>
 
@@ -58,29 +57,8 @@ export default function Contact() {
                 <Input
                   id="contact-cellphone"
                   placeholder="Insira seu número de celular"
-                  className="contact-input"
+                  className="bg-zinc-300"
                 />
-              </div>
-              <div className="contact-input-container">
-                <div className="flex items-center gap-1">
-                  <LayoutTemplate size={18} className="opacity-80" />
-                  <label htmlFor="contact-model">Modelo de site</label>
-                </div>
-                <Select>
-                  <SelectTrigger
-                    id="contact-model"
-                    className="flex items-center"
-                  >
-                    <SelectValue placeholder="Selecione um modelo" />
-                  </SelectTrigger>
-                  <SelectContent className="bg-transparent text-mainPalette-text backdrop-blur">
-                    <SelectItem value="blog">Blog</SelectItem>
-                    <SelectItem value="e-commerce">E-commerce</SelectItem>
-                    <SelectItem value="informativo">Informativo</SelectItem>
-                    <SelectItem value="landingpage">Landing Page</SelectItem>
-                    <SelectItem value="portfolio">Portfolio</SelectItem>
-                  </SelectContent>
-                </Select>
               </div>
             </div>
 
@@ -97,10 +75,11 @@ export default function Contact() {
               <Textarea
                 id="contact-message"
                 placeholder="Insira sua mensagem"
-                className="contact-input h-56 resize-none"
+                className="h-56 resize-none bg-zinc-300"
               />
             </div>
           </div>
+
           <button className="button-hover font-lg self-center rounded bg-mainPalette-primaryButton px-10 py-4 text-lg font-semibold uppercase tracking-wider text-mainPalette-text">
             Enviar
           </button>
